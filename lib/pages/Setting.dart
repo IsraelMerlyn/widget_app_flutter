a2644c9bbab3ec570d5f31ce4d53aecd05e985c6
IsraelMerlyn <merlyn142a@gmail.com>
2024-11-01 23:54:44 -0600

    tasks #0.1 Levantamiento de la Aplicacion

---
+++ lib/pages/Setting.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SettingPages extends StatefulWidget {
+  const SettingPages({super.key});
+
+  @override
+  State<SettingPages> createState() => _SettingPagesState();
+}
+
+class _SettingPagesState extends State<SettingPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
